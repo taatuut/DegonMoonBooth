ee94811cff2b23bed3e37ec0ce3ad46c72556521
Emil Zegers <e.zegers@upcmail.nl>
2020-06-10 00:51:39 +0200

    Clean up code.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -1,40 +1,27 @@
 var express = require('express');
 var router = express.Router();
-// request is deprecated
-//var request = require('request');
 const axios = require('axios');
  
 /* GET listCollections from backend. */
 router.get('/', (req, res, next) => {
-/*
-//  axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
-axios.get('https://postman-echo.com/ip')
-    .then(data => res.send(JSON.stringify(data)))
-    .catch(err => {
-      console.log(err),
-      next(err)
-  });
-*/
-
- axios.get('http://degonmoon-backend:8080/listCollections.js')
-//axios.get('https://postman-echo.com/ip')
-  .then((response) => {
-      console.log(response);
-      res.send(response.data);
-  })
-  .catch((error) => {
-      if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-      } else if (error.request) {
-          console.log(error.request);
-      } else {
-          console.log('Error', error.message);
-      }
-      console.log(error.config);
-  });
-
+  //axios.get('https://postman-echo.com/ip')
+  axios.get('http://degonmoon-backend:8080/listCollections.js')
+    .then((response) => {
+        console.log(response);
+        res.send(response.data);
+    })
+    .catch((error) => {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+    });
 })
 
 module.exports = router;
