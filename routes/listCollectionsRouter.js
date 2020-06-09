3a085406b081bfc3ef81a5b82be6e8ca99b36688
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 21:47:56 +0200

    Changed axios request code.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -7,18 +7,10 @@ const axios = require('axios');
 /* GET listCollections from backend. */
 router.get('/', function(req, res, next) {
   axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
-  .then(function (response) {
-    // handle success
-    res.send(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log('error:', error); // Print the error if one occurred and handle it
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  })
-  .finally(function () {
-    // always executed
+    .then(data => res.send(data))
+    .catch(err => next(err));
   });
 });
+
 
 module.exports = router;
