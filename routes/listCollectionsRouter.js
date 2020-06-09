e887a409fb536282cf00ee8f3ba67938ea69c7e8
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 20:55:09 +0200

    Replace deprecated request with axios.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
-
+// request is deprecated
+//var request = require('request');
+const axios = require('axios');
+ 
 /* GET listCollections from backend. */
 router.get('/', function(req, res, next) {
-  request('http://degonmoon-backend:8080/javascripts/listCollections.js', function (error, response, body) {
+  axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
+  .then(function (response) {
+    // handle success
+    res.send(response);
+  })
+  .catch(function (error) {
+    // handle error
     console.log('error:', error); // Print the error if one occurred and handle it
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.send(body)
+  })
+  .finally(function () {
+    // always executed
   });
 });
 
