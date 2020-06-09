368549496a4728a3d958f4979f84a6375c0ca1c1
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 22:24:42 +0200

    Removed port from backend service uri.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -6,7 +6,7 @@ const axios = require('axios');
  
 /* GET listCollections from backend. */
 router.get('/', (req, res, next) => {
-  axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
+  axios.get('http://degonmoon-backend/javascripts/listCollections.js')
     .then(data => res.send(data))
     .catch(err => next(err));
 })
