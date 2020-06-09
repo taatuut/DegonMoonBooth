1e73dff8027b6ea112fd883e9b75ba3242577650
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 22:34:16 +0200

    Changed uri to IP number and port.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -6,7 +6,7 @@ const axios = require('axios');
  
 /* GET listCollections from backend. */
 router.get('/', (req, res, next) => {
-  axios.get('http://degonmoon-backend/javascripts/listCollections.js')
+  axios.get('http://172.30.158.136:8080/javascripts/listCollections.js')
     .then(data => res.send(data))
     .catch(err => next(err));
 })
