28943a79259b4a9b90f29a43a0c6762429a1bb55
Emil Zegers <e.zegers@upcmail.nl>
2020-06-10 00:11:11 +0200

    Changed response.

--- routes/listCollectionsRouter.js
+++ routes/listCollectionsRouter.js
@@ -6,13 +6,34 @@ const axios = require('axios');
  
 /* GET listCollections from backend. */
 router.get('/', (req, res, next) => {
+/*
 //  axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
-  axios.get('https://postman-echo.com/ip')
+axios.get('https://postman-echo.com/ip')
     .then(data => res.send(JSON.stringify(data)))
     .catch(err => {
       console.log(err),
       next(err)
   });
+*/
+
+//  axios.get('http://degonmoon-backend:8080/javascripts/listCollections.js')
+axios.get('https://postman-echo.com/ip')
+  .then((res) => {
+      console.log(res);
+  })
+  .catch((error) => {
+      if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+      } else if (error.request) {
+          console.log(error.request);
+      } else {
+          console.log('Error', error.message);
+      }
+      console.log(error.config);
+  });
+
 })
 
 module.exports = router;
